530f100a727d29a70e21532d19c2ba2129f26232
na-parse <nathan@unit03.net>
2026-03-06 14:06:17 -0800

    update: Switch to bun, add deploy scripts

---
+++ deploy/update.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -e
+RUNDIR="$(pwd)"
+REPO="na-parse/passgen.git"
+INSTALL_DIR="/var/www/passgen"
+DOT_SERVICE="passgen.service"
+
+DEPLOY_DIR="${INSTALL_DIR}/deploy"
+SERVICE_SOURCE="${DEPLOY_DIR}/${DOT_SERVICE}"
+SYSTEMD_SERVICE="/etc/systemd/system/${DOT_SERVICE}"
+
+# Pre-run checks
+if [ ! -d "$INSTALL_DIR" ]; then
+    echo "FATAL: INSTALL_DIR \"$INSTALL_DIR\" does not exist/is not a dir"
+    exit 1
+fi
+
+# Reload systemd daemon in case service file was updated
+sudo systemctl daemon-reload
+
+
+# Check if a new pull is required
+cd "$INSTALL_DIR"
+COMMIT_LOCAL=$(git rev-parse HEAD)
+git fetch origin main --quiet
+COMMIT_REMOTE=$(git rev-parse origin/main)
+
+if [ "${COMMIT_LOCAL}" = "${COMMIT_REMOTE}" ]; then
+    echo "No updates, already at ${COMMIT_LOCAL:0:7}"
+    cd "$RUNDIR"
+    exit 0
+fi
+
+# Pull update and rebuild site
+echo "=== Update available: ${COMMIT_LOCAL:0:7} → ${COMMIT_REMOTE:0:7}"
+git pull origin main
+echo
+
+# Service File Setup/Validation (after pull so the file is present)
+if [[ ! -f "$SYSTEMD_SERVICE" ]]; then
+    echo "Installing systemd service file: $DOT_SERVICE"
+    sudo ln -s "$SERVICE_SOURCE" "$SYSTEMD_SERVICE"
+fi
+sudo systemctl daemon-reload
+
+echo "=== Rebuilding deployment ..."
+bun install --frozen-lockfile
+bun run build
+
+# Restart service
+echo "Restarting ${DOT_SERVICE} ..."
+sudo systemctl restart "$DOT_SERVICE" || RESTART_FAILED=true
+
+if [ "${RESTART_FAILED}" = "true" ]; then
+    echo "ERROR: Issue restarting ${DOT_SERVICE} !!!"
+    cd "$RUNDIR"
+    EXITVALUE=1
+else
+    EXITVALUE=0
+    sleep 5 # Give the server a moment to fully start before status output
+    echo "=== Service Restart Completed:"
+fi
+
+sudo systemctl status "${DOT_SERVICE}" -l --no-pager
+cd "$RUNDIR"
+exit $EXITVALUE
